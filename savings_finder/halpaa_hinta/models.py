9af05a720246e1ad38aee03a78c74670ce3cb377
xTech/beintouch01 <xtech.beintouch01@gmail.com>
2023-07-21 14:05:03 +0300

    change model, populate data for db infrastructure

--- savings_finder/halpaa_hinta/models.py
+++ savings_finder/halpaa_hinta/models.py
@@ -1,59 +1,59 @@
 from django.db import models
 from datetime import datetime, timedelta
+from django.utils import timezone
 
 class Users(models.Model):
     id = models.BigAutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=100, blank=True)
-    favourite_music = models.CharField(max_length=100, blank=True)
-    last_updated = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    name = models.CharField(max_length=100, default='John Doe')
+    email = models.CharField(max_length=100, default='beintouch01@gmail.com')
+    password = models.CharField(max_length=100, default='123456')
+    phone_number = models.CharField(max_length=100, blank=True, null=True)
+    last_updated = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.name 
     
 class Payments(models.Model):
-    # Users.payment_set.all()
-    user = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='payments', blank=True, null=True)
-    token = models.CharField(max_length=100)
-    payment_amount = models.IntegerField(default=0)
-    payment_date = models.DateField(auto_now_add=True)
-    invoice_number = models.AutoField(primary_key=True)
+    invoice_number = models.IntegerField(primary_key=True)
+    amount = models.IntegerField(default=0)
     payment_status = models.CharField(max_length=100, default='pending')
-    last_updated = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    currency = models.CharField(max_length=100, default='EUR')
+    payment_type = models.CharField(max_length=100, default='credit card')
+    transaction_date = models.DateField(default=datetime.now)
+    cardholder_name = models.CharField(max_length=100, default='John Doe')
+    card_number = models.CharField(max_length=100, default='0000 0000 0000 0000')
+    expiration_date = models.CharField(max_length=5, default='01/01')
+    cvv = models.CharField(max_length=3, default='000')
+    billing_address = models.CharField(max_length=100, default='Street 1, City, Country')
+    shipping_address = models.CharField(max_length=100, blank=True, null=True)
+    description = models.CharField(max_length=100, blank=True, null=True)
+    last_updated = models.DateTimeField(default=timezone.now)
+    user = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='payments', default=0)
 
     def __str__(self):
         return str(self.invoice_number)
     
 class Products(models.Model):
     id = models.BigAutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-    category = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, default='nimi')
+    category = models.CharField(max_length=100, default='All')
     description = models.CharField(max_length=100, blank=True, null=True)
-    image = models.CharField(max_length=1000, blank=True, null=True)
-    price1 = models.IntegerField(default=None, blank=True, null=True)
-    source1 = models.CharField(max_length=100)
-    price2 = models.IntegerField(default=None, blank=True, null=True)
-    source2 = models.CharField(max_length=100)
-    price3 = models.IntegerField(default=None, blank=True, null=True)
-    source3 = models.CharField(max_length=100)
-    # auto_now_add=True
-    last_updated = models.DateTimeField(default=datetime.now, blank=True, null=True)
- 
+    image1 = models.CharField(max_length=1000, blank=True, null=True)
+    price1 = models.IntegerField(blank=True, null=True)
+    source1 = models.CharField(max_length=1000, blank=True, null=True)
+    image2 = models.CharField(max_length=1000, blank=True, null=True)
+    price2 = models.IntegerField(blank=True, null=True)
+    source2 = models.CharField(max_length=1000, blank=True, null=True)
+    image3 = models.CharField(max_length=1000, blank=True, null=True)
+    price3 = models.IntegerField(blank=True, null=True)
+    source3 = models.CharField(max_length=1000, blank=True, null=True)
+    additional_info = models.CharField(max_length=1000, blank=True, null=True)
+    last_updated = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.name
     
-    def get_tori_price(self):
-        # Products.objects.filter(name__icontains='tuuletin').values()
-        tori_price = Products.objects.filter(name__icontains=self).values('price1', 'source1')
-        print('Queryset: ', tori_price.query)
-        return tori_price
 
-    def get_last_hours_prices(self):
-        last_hours_prices = Products.objects.filter(last_updated__gte=datetime.now()-timedelta(days=1)).values('price1', 'source1')
-        return last_hours_prices
 
 
 
